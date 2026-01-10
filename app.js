ee1a41ac0abf760c6d69240f0a6c08c2a89a8f49
AbdullayevTemurmalik <tmaq77@gamil,com>
2026-01-10 13:44:45 +0500

    Express-6dars Middleware

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const postModel = require("./models/post.model");
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use("/api/post", require("./routes/post.route"));
+
+const PORT = process.env.PORT || 8080;
+
+const bootstrap = async () => {
+  try {
+    await mongoose
+      .connect(process.env.DB_URL)
+      .then(() => console.log("Connected DB"));
+
+    app.listen(PORT, () =>
+      console.log(`Listening on - http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(`Error connecting with DB: ${error}`);
+  }
+};
+
+bootstrap();
